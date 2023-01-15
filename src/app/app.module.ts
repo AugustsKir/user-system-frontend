b4ef4f1804c317e22593e7908fa0f33e2d33224e
AugustsKir <kirsisaugusts@gmail.com>
2023-01-15 21:04:56 +0200

    improved ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import {RouterOutlet} from "@angular/router";
 import {AppRoutingModule} from "./app-routing-module";
 import {UsersComponent} from './pages/users/users.component';
 import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -13,10 +18,15 @@ import {HttpClientModule} from "@angular/common/http";
     UsersComponent
   ],
   imports: [
+    MatCardModule,
     BrowserModule,
     RouterOutlet,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
